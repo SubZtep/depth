3c0b13540b5b4a1dd5af2cc964113cfc8c75f396
Andras Serfozo <subztep@gmail.com>
2022-04-27 20:53:35 +0100

    After macro before build

--- lib/wc/src/d-theme.ts
+++ lib/wc/src/d-theme.ts
@@ -1,33 +1,21 @@
 import { statem } from "@depth/statem"
-// import preval from "preval.macro"
 import pug from "./pug.macro.cjs"
 
 const shadows = new WeakMap()
-// const html = preval`
-//   module.exports = require("pug").render(
-//     "label Background:\\n" +
-//     "  - var i = 0;\\n" +
-//     "  select\\n" +
-//     "    while i < 8\\n" +
-//     "      option(value=i) Colour ##{i++}"
-//   )
-// `
-
-const html = pug`
-label Background:
-  - var i = 0;
-  select
-    while i < 8
-      option(value=i) Colour ##{i++}
-`
 
 class DTheme extends HTMLElement {
   constructor() {
     super()
-    const root = this.attachShadow({ mode: "closed" })
-    root.innerHTML = html
-    shadows.set(this, root)
     statem("theme", { bg: 0 })
+    const root = this.attachShadow({ mode: "closed" })
+    shadows.set(this, root)
+    root.innerHTML = pug`
+    label Background:
+      - var i = 0;
+      select
+        while i < 8
+          option(value=i) Colour ##{i++}
+    `
   }
 
   connectedCallback() {
