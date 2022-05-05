d9c2c8d45b562fe92baf6763f5560aca220c8321
Andras Serfozo <subztep@gmail.com>
2022-05-05 09:23:18 +0100

    :wc: icon

---
+++ lib/wc/README.md
@@ -0,0 +1,24 @@
+# d-icon
+
+Display a responsive, inline SVG icon.
+
+Included shapes:
+- play
+- stop
+- hamburger
+
+## Properties
+
+| Property | Attribute | Type     | Description              |
+|----------|-----------|----------|--------------------------|
+| `name`   | `name`    | `String` | Reactive icon shape name |
+
+## CSS Custom Properties
+
+| Property        | Default        | Description      |
+|-----------------|----------------|------------------|
+| `--icon-fill`   | "currentColor" | Primary colour   |
+| `--icon-height` | "1rem"         | Inline font size |
+
+
+# d-theme
--- lib/wc/src/d-icon.ts
+++ lib/wc/src/d-icon.ts
@@ -3,8 +3,8 @@ import pug from "pug.macro"
 const style = `
 <style>
   :host svg {
-    height: 1rem;
-    fill: currentColor;
+    height: var(--icon-height, 1rem);
+    fill: var(--icon-fill, currentColor);
   }
   :host svg path:nth-of-type(2) {
     opacity: 0.4;
@@ -12,24 +12,66 @@ const style = `
 </style>
 `
 
-const icons = {
-  play: pug`
-    svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" role="img" aria-label="Play")
+const icons = new Map([
+  [
+    "play",
+    pug`svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" role="img" aria-label="Play")
       path(d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z")`,
-  stop: pug`
-    svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" role="img" aria-label="Stop")
+  ],
+  [
+    "stop",
+    pug`svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" role="img" aria-label="Stop")
       path(d="M384 128v255.1c0 35.35-28.65 64-64 64H64c-35.35 0-64-28.65-64-64V128c0-35.35 28.65-64 64-64H320C355.3 64 384 92.65 384 128z")`,
-  hamburger: pug`
-    svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" role="img" aria-label="Menu")
+  ],
+  [
+    "hamburger",
+    pug`svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" role="img" aria-label="Menu")
       path(d="M416 128H32C14.33 128 0 113.7 0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z")
       path(d="M0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256z")`,
-}
+  ],
+])
 
+/**
+ * Display a responsive, inline SVG icon.
+ *
+ * Included shapes:
+ * - play
+ * - stop
+ * - hamburger
+ *
+ * @element d-icon
+ *
+ * @prop {String} name - Reactive icon shape name
+ *
+ * @cssprop [--icon-height=1rem] - Inline font size
+ * @cssprop [--icon-fill=currentColor] - Primary colour
+ */
 class DIcon extends HTMLElement {
-  connectedCallback() {
-    const name = this.getAttribute("name")
-    if (!name || !Object.keys(icons).includes(name)) throw new Error(`Invalid icon name: ${name}`)
-    this.attachShadow({ mode: "closed" }).innerHTML = style + icons[name]
+  #root: ShadowRoot
+
+  static get observedAttributes() {
+    return ["name"]
+  }
+
+  attributeChangedCallback(name: string, oldValue: string | null, newValue: string) {
+    switch (name) {
+      case "name":
+        const svgEl = this.#root.lastElementChild!
+        if (!icons.has(newValue)) {
+          if (oldValue && icons.has(oldValue)) svgEl.outerHTML = pug`svg`
+          throw new Error(`Invalid icon shape name: ${newValue}`)
+        }
+        svgEl.outerHTML = icons.get(newValue)
+        break
+      default:
+        throw new Error(`Unknown attribute: ${name}`)
+    }
+  }
+
+  constructor() {
+    super()
+    this.#root = this.attachShadow({ mode: "closed" })
+    this.#root.innerHTML = style + pug`svg`
   }
 }
 
